82b89df1fa294e48282e2b36a1f866968784e863
Med_Wahbi <wahbisj@gmail.com>
2022-03-27 01:10:52 +0100

    update

---
+++ project1.js
@@ -0,0 +1,17 @@
+var text =document.getElementById("text");
+var save =document.getElementById("save");
+var delet =document.getElementById("delet");
+var textf =document.getElementById("textf");
+
+save.onclick=()=>{
+    textf.innerHTML=document.getElementById("text").value;
+    localStorage.setItem("nas",text.value);
+}
+delet.onclick=()=>{
+    textf.innerHTML="";
+}
+onload=()=>{
+    text.value=localStorage.getItem("nas");
+    textf.innerHTML=localStorage.getItem("nas");
+
+}
