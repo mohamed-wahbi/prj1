92918e017b6e82590d1346a8dad397ba78cd09c7
mohamed-wahbi <95344703+mohamed-wahbi@users.noreply.github.com>
2022-04-21 10:12:05 +0200

    Update project1.js

--- project1.js
+++ project1.js
@@ -2,12 +2,20 @@ var text =document.getElementById("text");
 var save =document.getElementById("save");
 var delet =document.getElementById("delet");
 var textf =document.getElementById("textf");
+var stor;
 
 save.onclick=()=>{
     textf.innerHTML=document.getElementById("text").value;
-    localStorage.setItem("nas",text.value);
+    var stor=localStorage.setItem("nas",text.value);
 }
 delet.onclick=()=>{
     textf.innerHTML="";
 }
+onload=()=>{
+    localStorage.clear(stor);
+   
+ 
+    //text.value=localStorage.getItem("nas");
 
+   // textf.innerHTML=localStorage.getItem("nas");
+}
